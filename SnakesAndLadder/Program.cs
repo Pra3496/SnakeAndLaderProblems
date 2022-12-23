ac919356ee5fb33cbf1a58359a7cdcd76f0601bd
Pranav Waghmare <piyushw55@gmail.com>
2022-12-23 14:36:28 +0530

    UC1 Single Player Position Zero

---
+++ SnakesAndLadder/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SnakesAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pos = 0; // position
+            
+            Console.WriteLine("Welcome! to Snake and Ladder Game");
+
+            //Position Initializing
+
+            Console.WriteLine("The position of player starts: " +pos);
+
+        }
+    }
+}
