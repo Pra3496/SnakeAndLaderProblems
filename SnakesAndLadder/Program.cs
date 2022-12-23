c0e6910c0c98e28e42a62044b65366dab289aee4
Pranav Waghmare <piyushw55@gmail.com>
2022-12-23 15:04:27 +0530

    UC3 Check for an option

--- SnakesAndLadder/Program.cs
+++ SnakesAndLadder/Program.cs
@@ -4,18 +4,35 @@
     {
         static void Main(string[] args)
         {
-            int pos = 0;  // Position
-            int dice = 0; // Dice
+            int pos = 0;       // Position Initializin
+            int dice = 0;      // Dice 
+            int opt = 0;       // options
+
+
+            Console.WriteLine("_______| Welcome! to Snake and Ladder Game |_______\n");
+          
+            Random random = new Random();       //Creating object random
+            dice = random.Next(1, 7);       // Using random method 
+            Console.WriteLine("The postition of player starts: {0} and random number of Dice: {1}\n", pos, dice);
             
-            Console.WriteLine("Welcome! to Snake and Ladder Game");
+            
+            Random random1 = new Random();      //Creating object random
+            opt = random1.Next(1, 4);        //Using random method
 
-            //Position Initializing
-
-            Random random = new Random();
-
-            dice = random.Next(1, 7);
-
-            Console.WriteLine("The position of player starts: {0} and random number of Dice: {1}",pos,dice);
+            switch (opt)         //Checking for options using switch case
+            {
+                case 1:
+                    pos = pos + dice;
+                    Console.WriteLine("The player moves ahead by number Dice value : " + pos);
+                    break;
+                case 2:
+                    pos= pos - dice;
+                    Console.WriteLine("The player moves behind by number Dice value : " + pos);
+                    break;
+                default:
+                    Console.WriteLine("The player stay at same place : " + pos);
+                    break;
+            }
 
         }
     }
